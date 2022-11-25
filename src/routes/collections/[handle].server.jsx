c237b9f67abd2aad071eb059dd117352bdd1f42c
casey.Q.Wood <caseywood252@gmail.com>
2022-11-25 12:35:53 -0800

    added SEO, analytics and collection description to Collection component

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -1,16 +1,71 @@
-import { useRouteParams } from "@shopify/hydrogen"
-import { Layout } from "../../components/Layout.server"
+import {
+  gql,
+  useShopQuery,
+  Seo,
+  useServerAnalytics,
+  useRouteParams,
+  ShopifyAnalyticsConstants,
+} from "@shopify/hydrogen";
+import { Suspense } from "react";
 
-export default function Collections() {
-  const {handle} = useRouteParams();
+import { Layout } from "../../components/Layout.server";
 
-  console.log("This is Handle: ", handle)
+export default function Collection() {
+  console.log("Calling Collections")
+  const { handle } = useRouteParams();
+
+  const {
+    data: { collection },
+  } = useShopQuery({
+    query: QUERY,
+    variables: {
+      handle,
+    },
+  });
+
+  useServerAnalytics({
+    shopify: {
+      pageType: ShopifyAnalyticsConstants.pageType.collection,
+      resourceId: collection.id,
+    },
+  });
 
   return (
     <Layout>
-      <section className="p-6 md:p-8 lg:p-12">
-        This will be the collection page for <strong>{handle}</strong>
-      </section>
+      <Suspense>
+        <Seo type="collection" data={collection} />
+      </Suspense>
+      <header className="grid w-full gap-8 p-4 py-8 md:p-8 lg:p-12 justify-items-start">
+        <h1 className="text-4xl whitespace-pre-wrap font-bold inline-block">
+          {collection.title}
+        </h1>
+
+        {collection.description && (
+          <div className="flex items-baseline justify-between w-full">
+            <div>
+              <p className="max-w-md whitespace-pre-wrap inherit text-copy inline-block">
+                {collection.description}
+              </p>
+            </div>
+          </div>
+        )}
+      </header>
     </Layout>
-  )
+  );
 }
+
+// The `Seo` component uses the collection's `seo` values, if specified. If not
+// specified, then the component falls back to using the collection's `title` and `description`.
+const QUERY = gql`
+  query CollectionDetails($handle: String!) {
+    collection(handle: $handle) {
+      id
+      title
+      description
+      seo {
+        description
+        title
+      }
+    }
+  }
+`;
