c68bc6a492a0ec96dd696b17c9eaa50f880a8e10
casey.Q.Wood <caseywood252@gmail.com>
2022-11-25 12:47:24 -0800

    implement collection product query and hand off to cards

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -1,17 +1,17 @@
 import {
   gql,
   useShopQuery,
-  Seo,
-  useServerAnalytics,
   useRouteParams,
+  useServerAnalytics,
   ShopifyAnalyticsConstants,
+  Seo,
 } from "@shopify/hydrogen";
-import { Suspense } from "react";
 
 import { Layout } from "../../components/Layout.server";
+import ProductCard from "../../components/ProductCard.server";
+import { Suspense } from "react";
 
 export default function Collection() {
-  console.log("Calling Collections")
   const { handle } = useRouteParams();
 
   const {
@@ -50,12 +50,18 @@ export default function Collection() {
           </div>
         )}
       </header>
+
+      <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {collection.products.nodes.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
     </Layout>
   );
 }
 
-// The `Seo` component uses the collection's `seo` values, if specified. If not
-// specified, then the component falls back to using the collection's `title` and `description`.
 const QUERY = gql`
   query CollectionDetails($handle: String!) {
     collection(handle: $handle) {
@@ -65,6 +71,40 @@ const QUERY = gql`
       seo {
         description
         title
+      }
+      image {
+        id
+        url
+        width
+        height
+        altText
+      }
+      products(first: 8) {
+        nodes {
+          id
+          title
+          publishedAt
+          handle
+          variants(first: 1) {
+            nodes {
+              id
+              image {
+                url
+                altText
+                width
+                height
+              }
+              priceV2 {
+                amount
+                currencyCode
+              }
+              compareAtPriceV2 {
+                amount
+                currencyCode
+              }
+            }
+          }
+        }
       }
     }
   }
